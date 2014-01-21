4dc469e6856269e92e5842673a63235bbb592213
xueer <841706256@qq.com>
2014-01-21 22:35:22 +0800

    Update 2014-1-19-homewor-02.py

--- 2014-1-19-homewor-02.py
+++ 2014-1-19-homewor-02.py
@@ -1,9 +1,11 @@
 #写一个猜数字游戏。每次随机1到100里面随机抽一个数，让用户输入，用户输入的比那个数大，就打印large，小的话就打印small，如果正确就提示用户，you win
+#coding=UTF-8
+import random
 num2 = random.randint(1,101)
 #print num2
 
 while True:
-	num1 = int(raw_input("游戏开始,请输入所猜数字:"))
+	num1 = int(raw_input("游戏开始,请输入数字:"))
 	if num1 > num2:
 		print "The guess is large."
 	elif num1 < num2:
